ecde4f2fa7a189ea5243075c4d4eedba50e5e1de
itihon <kalabin.alexandr@gmail.com>
2025-12-09 18:25:58 +0500

    refactor(messabebox): use Modal component

--- src/shared/ui/MessageBox/MessabeBox.tsx
+++ src/shared/ui/MessageBox/MessabeBox.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AppButton from '../AppButton/AppButton';
+import Modal from '../Modal/Modal';
 import classes from './MessabeBox.module.scss';
 
 interface MessageBoxProps {
@@ -19,16 +20,16 @@ const MessageBox: React.FC<MessageBoxProps> = ({ title, message, icon, onClose =
 
   return (
     visible 
-    ?  <div className={classes.overlay}>
-        <div className={classes.MessageBox}>
-          <h3 className={classes.title}>{title}</h3>
-          <div className={classes.content}>
-            <div className={classes.icon}>{icon}</div>
-            <span className={classes.message}>{message}</span>
+    ?  <Modal>
+          <div className={classes.MessageBox}>
+            <h3 className={classes.title}>{title}</h3>
+            <div className={classes.content}>
+              <div className={classes.icon}>{icon}</div>
+              <span className={classes.message}>{message}</span>
+            </div>
+            <AppButton className={classes.button} onClick={handleClick}>Close</AppButton>
           </div>
-          <AppButton className={classes.button} onClick={handleClick}>Close</AppButton>
-        </div>
-      </div>
+      </Modal>
     : ''
   );
 };
