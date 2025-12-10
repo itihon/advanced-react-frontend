fb37a76d84b4cac941e296ca1c71d277bfd64f3a
itihon <kalabin.alexandr@gmail.com>
2025-12-10 18:20:04 +0500

    refactor(navbar): add sign in link

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Navbar.module.scss';
-import routeConfig, { AppRoutes } from 'shared/config/routeCounfig/routeConfig';
 import { AppLink, AppLinkTheme, ThemeSwitcher } from 'shared/ui';
 import LangSwitcher from 'shared/ui/LangSwitcher/LangSwitcher';
 import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
+import { SigninForm } from 'features/AuthByUserName';
 
 const Navbar: React.FC = () => {
+  const [signinFormVisible, setSigninFormVisible] = useState(false);
   const { t } = useTranslation();
-  const location = useLocation();
+
+  const openSigninForm = () => {
+    setSigninFormVisible(true);
+  };
+
+  const closeSigninForm = () => {
+    setSigninFormVisible(false);
+  };
 
   return (
     <div className={classes.Navbar}>
+
       <div className={classes.switchers}>
         <ThemeSwitcher className={classes.switcher}/>
         <LangSwitcher className={classes.switcher}/>
       </div>
+
+      <AppLink 
+        isActive={false}
+        theme={AppLinkTheme.INVERTED}
+        to={'#'}
+        onClick={openSigninForm}>
+          {t('navbar.enter')}
+      </AppLink>
+
       {
-        Object.entries(routeConfig)
-          .map(
-            ([name, props], idx) => name !== AppRoutes.NOT_FOUND  
-              ? <AppLink 
-                  isActive={location.pathname === props.path}
-                  theme={AppLinkTheme.INVERTED}
-                  key={idx} 
-                  to={props.path}>
-                    {t(`navbar.${name}`)}
-                </AppLink>
-              : ''
-          )
+        signinFormVisible 
+          ? <SigninForm close={closeSigninForm}/>
+          : ''
       }
     </div>
   );
