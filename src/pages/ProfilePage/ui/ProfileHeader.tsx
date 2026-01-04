cab300f63dbaf821b1776d7f35d82470291ceac5
itihon <kalabin.alexandr@gmail.com>
2026-01-04 15:57:56 +0500

    refactor(profileheader.tsx): add cancel and save buttons

--- src/pages/ProfilePage/ui/ProfileHeader.tsx
+++ src/pages/ProfilePage/ui/ProfileHeader.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { AppButton } from 'shared/ui';
 import classes from './ProfileHeader.module.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from 'app/providers/StoreProvider/config/store';
-import { setProfileReadOnly } from 'entities/Profile';
+import { getProfileReadOnly, setProfileReadOnly } from 'entities/Profile';
 
 const ProfileHeader: React.FC = () => {
   const { t } = useTranslation('profile-page');
   const dispatch = useDispatch<AppDispatch>();
+  const readOnly = useSelector(getProfileReadOnly);
 
   const onEditClick = () => {
     dispatch(setProfileReadOnly(false));
+  };
+
+  const onCancelClick = () => {
+
+  };
+
+  const onSaveClick = () => {
+
   };
 
   return (
     <>
       <div className={classes.ProfileHeader}>
         <h2>{t('profile-page:header')}</h2>
-        <AppButton size='size-l' onClick={onEditClick}>{'🖊  ' + t('edit')}</AppButton>
+        {
+          readOnly
+            ? <AppButton size='size-l' onClick={onEditClick}>{'🖊  ' + t('edit')}</AppButton>
+            : <div className={classes['button-row']}>
+              <AppButton size='size-l' onClick={onCancelClick}>{'❌  ' + t('cancel')}</AppButton>
+              <AppButton size='size-l' onClick={onSaveClick}>{'💾  ' + t('save')}</AppButton>
+            </div>
+        }
       </div>
     </>
   );
