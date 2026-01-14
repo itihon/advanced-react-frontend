0db5505f870de226dd2631365dc4e3cfaf49721d
itihon <kalabin.alexandr@gmail.com>
2026-01-14 21:03:58 +0500

    refactor(appinput.tsx): add className prop

--- src/shared/ui/AppInput/AppInput.tsx
+++ src/shared/ui/AppInput/AppInput.tsx
@@ -1,5 +1,6 @@
 import React, { HTMLInputTypeAttribute, memo } from 'react';
 import classes from './AppInput.module.scss';
+import classNames from 'classnames';
 
 interface AppInputProps {
   type: HTMLInputTypeAttribute;
@@ -8,11 +9,12 @@ interface AppInputProps {
   name?: string;
   readOnly?: boolean;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  className?: string;
 }
 
-const AppInput: React.FC<AppInputProps> = ({ type, label, value, name, onChange, readOnly }) => {
+const AppInput: React.FC<AppInputProps> = ({ type, label, value, name, onChange, readOnly, className }) => {
   return (
-    <div className={classes.AppInput}>
+    <div className={classNames(classes.AppInput, className)}>
       <label className={classes.label} htmlFor={name}>{label}</label>
       <input readOnly={readOnly} type={type} className={classes.input} onChange={onChange} name={name} id={name} value={value} />
     </div>
