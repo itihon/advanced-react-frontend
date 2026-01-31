493efa239b33cdbbf72388220fa4ccb30a6ab498
itihon <kalabin.alexandr@gmail.com>
2026-01-31 17:09:40 +0500

    refactor(appinput.tsx): add ref prop

--- src/shared/ui/AppInput/AppInput.tsx
+++ src/shared/ui/AppInput/AppInput.tsx
@@ -10,13 +10,14 @@ interface AppInputProps {
   readOnly?: boolean;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   className?: string;
+  ref?: React.RefObject<HTMLInputElement | null>;
 }
 
-const AppInput: React.FC<AppInputProps> = ({ type, label, value, name, onChange, readOnly, className }) => {
+const AppInput: React.FC<AppInputProps> = ({ ref, type, label, value, name, onChange, readOnly, className }) => {
   return (
     <div className={classNames(classes.AppInput, className)}>
       <label className={classes.label} htmlFor={name}>{label}</label>
-      <input readOnly={readOnly} type={type} className={classes.input} onChange={onChange} name={name} id={name} value={value} />
+      <input ref={ref} readOnly={readOnly} type={type} className={classes.input} onChange={onChange} name={name} id={name} value={value} />
     </div>
   );
 };
