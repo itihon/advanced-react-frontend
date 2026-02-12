f1959d05b5969a49a2254b818681dcd5fb5a30b8
itihon <kalabin.alexandr@gmail.com>
2026-02-12 19:36:17 +0500

    chore(storybook): add missing api url variable definition

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -18,6 +18,7 @@ const config: StorybookConfig = {
       }),
       new webpack.DefinePlugin({
         __IS_DEV__: JSON.stringify(isDev),
+        __API_URL__: JSON.stringify('http://localhost:3031'),
       }),
       new CopyWebpackPlugin({
         patterns: [
